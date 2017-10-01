d46ee4c17bbeff718ca0ca60691fd39a43e2aafc
Martina_Shebova <martina2314@gmail.com>
2017-10-01 16:48:37 +0300

    Added some functionalities

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
+import { home } from 'homeController';
 (function routing() {
 
     let router = new Navigo(null, true);
         router.on('/', () => {
             console.log(4);
         })
+        .on('/home', home)
         .resolve();
 }()); 
--- public/js/system-config.js
+++ public/js/system-config.js
@@ -5,14 +5,13 @@ System.config({
         'plugin-babel': '/libs/systemjs-plugin-babel/plugin-babel.js',
         'systemjs-babel-build': '/libs/systemjs-plugin-babel/systemjs-babel-browser.js',
 
-        // External Libraries
-        'jquery': '/libs/jquery/dist/jquery.min.js',
-        'bootstrap-js': '/libs/bootstrap/dist/js/bootstrap.min.js',
-
         // App files
         'main': '/static/js/main.js',
         'requester': '/static/js/requester.js',
         'templates': '/static/js/templates.js',
+
+        //Controllers
+        'homeController': '/static/js/controllers/homeController.js'
 
     }
 });
