a6e938b02019bc7af27e0aa70d1798d1018fbed7
Martina_Shebova <martina2314@gmail.com>
2017-10-08 17:03:51 +0300

    Half of the work on responsiveness is done

--- public/js/helpers/mobile.js
+++ public/js/helpers/mobile.js
@@ -1,21 +1,38 @@
-(function () {
+// Initial resolution detection
+
+let view = $(window).width();
+
+if (view < 768) {
+    $('body').addClass('small-devices');
+}
+
+if (view > 768 && view < 956) {
+    
+    $('body').addClass('medium-devices');
+}
+
+if(view > 992){
+    
+    $('body').removeClass();        
+}
+
+//Detect on resize
+
+$(window).on('resize', function() {
 
     let view = $(window).width();
 
-    if (view < 768) {
-        $('body').addClass('extra-small-device');
+    if(view < 768) {
+        $('body').removeClass();  
+        $('body').addClass('small-devices');
     }
 
-    if (view > 768 && view < 992) {
-        $('body').addClass('small-device');
+    if (view > 768 && view < 956) {
+        $('body').removeClass();  
+        $('body').addClass('medium-devices');
     }
 
-    if (view > 992 && view < 1200) {
-        $('body').addClass('medium-device');
-    }
-
-    if(view > 1200){
+    if(view > 992){
         $('body').removeClass();        
     }
-
-})();
+});
